412b6558b4773c31f3a89b328bdf98d418f4d3f5
thanosbalo <thanosbalogiannis@gmail.com>
2022-10-30 13:06:04 +0200

    Code that passes all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(...args) {
+    let fargs=args.length;
+    let array=args[0];
+    let deleteArray=[];
+    for (let k=1;k<fargs;k++){
+        deleteArray[k-1]=args[k];
+    }
+    let arrayLength=array.length;
+    for (let x=0;x<deleteArray.length;x++){
+        for (let i=0;i<arrayLength;i++){
+        if (array[i]===deleteArray[x]){
+            array.splice(i,1);
+        }
+    }
+    }
+    return array;
 };
 
 // Do not edit below this line
